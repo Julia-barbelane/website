154efb739cd93cb8e49a99391aec4de571b8d71e
Travis CI <stephane.langlois@scopyleft.fr>
2017-03-13 22:05:02 +0100

    Add agenda

--- dist/dk.js
+++ dist/dk.js
@@ -602,7 +602,7 @@ layout.folders.html('\n<main class="container">\n  <div id="breadcrumb" class="b
 *
 */
 layout.home = new Layout('home');
-layout.home.html('\n<main>\n  <section class="home-intro">\n      <div class="home-intro-content container">\n        <h2>' + MULTIBAO.UVP1 + '<span>' + MULTIBAO.UVP2 + '</span></h2>\n        <div>\n          <a href="#multibao/contributions/blob/master/pages/commencer_ici.md">' + MULTIBAO.BUTTON1 + '</a>\n        </div>\n        <div>\n          <a href="#multibao/documentation/blob/master/README.md">' + MULTIBAO.BUTTON2 + '</a>\n        </div>\n      </div>\n  </section>\n  <section id="gh-crew-list" class="container">\n    <ul data-template="crews">\n    </ul>\n  </section>\n</main>');
+layout.home.html('\n<main>\n  <section class="home-intro">\n      <div class="home-intro-content container">\n        <h2>' + MULTIBAO.UVP1 + '<span>' + MULTIBAO.UVP2 + '</span></h2>\n        <div>\n          <a href="' + MULTIBAO.LINK1 + '">' + MULTIBAO.BUTTON1 + '</a>\n        </div>\n        <div>\n          <a href="' + MULTIBAO.LINK2 + '">' + MULTIBAO.BUTTON2 + '</a>\n        </div>\n      </div>\n  </section>\n  <section id="gh-crew-list" class="container">\n    <ul data-template="crews">\n    </ul>\n  </section>\n</main>');
 'use strict';
 
 /**
